40c471bb6bd053beab9ff0571e913a87e09035c7
Warren R. Bank <warren.r.bank@gmail.com>
2026-02-04 17:12:20 -0800

    [Android < 3.0] disable PreferenceFragment and show Toast

--- android-studio-project/WebViewClientObserver/src/main/java/com/github/warren_bank/webview_client_observer/settings/SettingsActivity.java
+++ android-studio-project/WebViewClientObserver/src/main/java/com/github/warren_bank/webview_client_observer/settings/SettingsActivity.java
@@ -1,12 +1,26 @@
 package com.github.warren_bank.webview_client_observer.settings;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.widget.Toast;
 
 public class SettingsActivity extends PreferenceActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+
+    if (Build.VERSION.SDK_INT >= 11) {
+      getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+    }
+    else {
+      Toast.makeText(
+        SettingsActivity.this,
+        "Android 3.0 or higher is required to edit the app settings",
+        Toast.LENGTH_SHORT
+      ).show();
+
+      finish();
+    }
   }
 }
